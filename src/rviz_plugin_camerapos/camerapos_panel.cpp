32b57e0b1d13b40828f427d6e1de53d98d7acb16
HiraiKyo <kyo.mikhael@gmail.com>
2024-04-30 18:04:30 +0900

    Draft: onInitialize等の動作確認

--- src/rviz_plugin_camerapos/camerapos_panel.cpp
+++ src/rviz_plugin_camerapos/camerapos_panel.cpp
@@ -14,6 +14,7 @@ namespace rviz_plugin_camerapos
 
     void CameraposPanel::onInitialize()
     {
+        ROS_INFO("TEST");
         pub_ = nh_.advertise<std_msgs::Float64>("camerapos", 1);
         parentWidget()->setVisible(true);
     }
@@ -28,6 +29,61 @@ namespace rviz_plugin_camerapos
     {
         hide();
         parentWidget()->hide();
+    }
+
+    void CameraposPanel::load( const rviz::Config& config)
+    {
+        // rviz configから読み込み
+        rviz::Panel::load( config );
+        pos_list_config_ = config.mapGetChild("pos_list");
+        
+        // ツリー更新
+        CameraposPanel::refreshTree();        
+    };
+    
+    void CameraposPanel::save(rviz::Config config) const
+    {
+
+    };
+
+    void CameraposPanel::newPos(std::string name, std::vector<float> position, std::vector<float> rotation) {
+        // 新しい子エントリを作成
+        rviz::Config position_config = pos_list_config_.listAppendNew();
+        QString qname = QString::fromStdString(name);
+        // 各キーと値を保存
+        position_config.mapSetValue("name", qname);
+        position_config.mapSetValue("x", position[0]);
+        position_config.mapSetValue("y", position[1]);
+        position_config.mapSetValue("z", position[2]);
+        position_config.mapSetValue("rx", rotation[0]);
+        position_config.mapSetValue("ry", rotation[1]);
+        position_config.mapSetValue("rz", rotation[2]);
+
+        // ツリー更新
+        CameraposPanel::refreshTree();
+    }
+
+    void CameraposPanel::refreshTree(){
+        // ツリーをクリア
+        ui_->treeWidget->clear();
+
+        // 当クラスに保持されたConfigからツリーを再構成
+        for(int32_t i = 0; i < pos_list_config_.listLength(); ++i){
+            rviz::Config position_config = pos_list_config_.listChildAt(i);
+            QString qname;
+            float x, y, z, rx, ry, rz;
+
+            if (position_config.mapGetString("name", &qname) &&
+                position_config.mapGetFloat("x", &x) &&
+                position_config.mapGetFloat("y", &y) &&
+                position_config.mapGetFloat("z", &z) &&
+                position_config.mapGetFloat("rx", &rx) &&
+                position_config.mapGetFloat("ry", &ry) &&
+                position_config.mapGetFloat("rz", &rz)) {
+                QTreeWidgetItem* item = new QTreeWidgetItem(ui_->treeWidget);
+                item->setText(0, qname);
+            }
+        }
     }
 }
 PLUGINLIB_EXPORT_CLASS(rviz_plugin_camerapos::CameraposPanel, rviz::Panel)
--- src/rviz_plugin_camerapos/camerapos_panel.h
+++ src/rviz_plugin_camerapos/camerapos_panel.h
@@ -23,14 +23,23 @@ namespace rviz_plugin_camerapos
             void onInitialize() override;
             void onEnable();
             void onDisable();
+            virtual void load(const rviz::Config& config);
+            virtual void save(rviz::Config config) const;
 
+            void newPos(std::string name, std::vector<float> position, std::vector<float> rotation);
+
+            void refreshTree();
+            
         private Q_SLOTS:
 
         protected:
             Ui::MainUI* ui_;
             int value_{0};
             std::string topic_name_{"camerapos"};
+            
             ros::NodeHandle nh_;
             ros::Publisher pub_;
+    
+            rviz::Config pos_list_config_;
     };
 }
